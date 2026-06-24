b8fdad2125528b767bd6e6aafd3f158150e2b18d
youssef kamal <y.ali2242@nu.edu.eg>
2026-06-24 09:42:33 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -21,7 +21,7 @@ export const NGROK_WEB_BASE =
   localStorage.getItem("WEB_BASE")?.trim()?.replace(/\/$/, "") ||
   "https://lianne-sauciest-cory.ngrok-free.dev";
 
-export const WEB_APP_PATH = "/GradWep";
+export const WEB_APP_PATH = "GradWep/";
 
 export const WEB_BASE =
   (location.hostname === "127.0.0.1" || location.hostname === "localhost")
